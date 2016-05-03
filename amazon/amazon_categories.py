cde1abf7155ad8588e28a268aa3f63d46e7cfd02
zinspierre <zins.pierre@gmail.com>
2016-05-03 19:22:15 +0200

    mise a jour

--- amazon/amazon_categories.py
+++ amazon/amazon_categories.py
@@ -3,125 +3,92 @@ from bs4 import BeautifulSoup
 import time
 from random import randint
 
+# file
+# urls = [("Ballons", "B0116Q6U5U","https://www.amazon.fr/adidas-Replique-Ballon-Bright-Indigo/dp/B0116Q6U5U/ref=lp_339893031_1_1?s=sports&ie=UTF8&qid=1462282939&sr=1-1")]
+urls = [("Ordinateurs portables", "B0105LFO3G","http://www.amazon.fr/Asus-Chromebook-C200MA-KX017-Portable-Celeron/dp/B0105LFO3G/ref=sr_1_4?s=computers&ie=UTF8&qid=1459258627&sr=1-4&keywords=pc")]
 
-urls = [("Ordinateurs portables","http://www.amazon.fr/Asus-Chromebook-C200MA-KX017-Portable-Celeron/dp/B0105LFO3G/ref=sr_1_4?s=computers&ie=UTF8&qid=1459258627&sr=1-4&keywords=pc")]
-
-
-class Category:
-	def __init__(self):
-		self.keywords = []
-	
-	def addWords(self, w):
-		self.keywords.append(w)	
-
-	def __str__(self):
-		res = ""
-		for i in self.keywords:
-			res += i + " "
-		return res
-
-class Node:
-	def __init__(self, name):
-		self.name = name
-
-	def __str__(self):
-		return self.name
-
-	def __eq__(self, other):
-		return (isinstance(other, self.__class__) and self.name == other.name)
 
 nodes = []
-
 edges = []
-# n = Node("name")
-# m = Node("aaaname")
-# r = Node("namsdfe")
-# nodes.append(n)
-# nodes.append(m)
-# nodes.append(r)
-# print(nodes)
-# print(nodes.index('aaaname'))
-# exit()
 
+# urls visités
 old_urls = []
 
-f = open('res.txt','w')
-
+# fichier sortie
+f = open('tmp_res.txt','w')
 
 counter = 0
 while len(urls) != 0:
+
+	# delai attente au bout de 100 itérations
 	counter += 1
 	if counter % 100 == 0:
 		random = randint(0, 100)
 		time.sleep(random)
 
-
-	# print(counter)
+	# catégorie du lien début de file
 	prev_cate = urls[0][0]
-
-	url = urls[0][1]
+	# reference du lein debut file
+	prev_ref = urls[0][1]
+	# url début de file
+	url = urls[0][2]
+	# pop file
 	urls.pop(0)
+
+	# si le lien a déjà été visité => itération suivante
 	if url in old_urls:
 		continue
+	# ajout du nouveau lien dans la file
 	old_urls.append(url)
+
+	#recupération de la reference
+	begin = url.find("dp")
+	begin += 3
+	end1 = url.find("/", begin)
+	end2 = url.find("?", begin)
+	end = min(end1, end2)
+	if end < 0:
+		end = max(end2, end1)
+	ref = url[begin:end]
 
 
 	r = requests.get(url)
 	soup = BeautifulSoup(r.content, "html.parser")
-	# print(soup.prettify())
-	# div = soup.find_all("div", {"aria-hidden":"true"})
-	# div = soup.find_all("div", {"class":"p13n-sc-truncated"})
 	try:
+		#object souvent achetés
 		div = soup.find_all("li", {"class":"a-carousel-card"})
+
+		#catégorie de page courante
 		cate = soup.find("ul", {"class":"a-horizontal a-size-small"})
 		cates = cate.find_all("a")
-	
-		# category = Category()
+		# cates : différent niveau de catégorie (on veutl le dernier)
+		# récupération de la vraie catégorie
 		category = cates[-1].text.strip()
 
+		# si la catégorie de la page est la même que celle de l'objet qui nous a fait arrivé sur cette page, on l'ignore
+		#counter > 1 : sinon probleme à première itération
 		if category == prev_cate and counter > 1:
 			continue
 
 
-		# save node (categorie)
-		n = Node(category)
-		if not n in nodes:
-			nodes.append(n)
+		# si pas déjà ajout de new category dans les nodes 
+		if not category in nodes:
+			nodes.append(category)
+
+
 		
+		#ajout edge
 		edges.append((prev_cate, category))
-		s = prev_cate + ";" + category + "\n"
-		f.write(s) # python will convert \n to os.linesep
+		#write edge to file
+		s = prev_ref + ";" + ref + ";" + prev_cate + ";" + category + "\n"
+		f.write(s)
+
 		#ajout des autres liens dans la file
 		for i in div:
 			link = i.find("a")
 			txt_link = link.get("href")
-			urls.append((category, "http://www.amazon.fr" + txt_link))
+			urls.append((category, ref, "http://www.amazon.fr" + txt_link))
 
-		# fin
-		# if counter > 200:
-		# 	break
 	except:
 		print('No link in this page')
 f.close()
-
-# f = open('res.txt','w')
-# for i in edges:
-# 	s = i[0] + " ; " + i[1] + "\n"
-# 	f.write(s)
-# f.close()
-
-
-
-# urls = ["http://www.amazon.fr/gp/site-directory/ref=nav_shopall_btn"]
-
-
-
-# url = urls[0]
-# r = requests.get(url)
-# soup = BeautifulSoup(r.content)
-# body = soup.find("table", {"id": "shopAllLinks"})
-# h2 = body.find_all("li");
-
-# for i in h2:
-# 	print(i.text)
-# exit();
--- amazon/parsing.py
+++ amazon/parsing.py
@@ -1,50 +1,55 @@
 from lxml import etree
 
+input_file = "resultat_1h30"
+output_file = "amazon_gephi2.gexf"
+
+
+# categories == nodes
 categories = []
+# links == edges
 links = []
 
 
-
-f = open("resultat_1h30")
-# g = open("test", "w")
+#fichier avec les données
+f = open(input_file)
+#récupération des catégories == nodes
 for i in f:
 	tab = i.partition(";")
-	#tab = str.split(i, ";")
-
 	if not tab[0] in categories:
 		categories.append(tab[0])
 	if not tab[2].rstrip() in categories:
 		categories.append(tab[2].rstrip())
 
-# for i in categories:
-# 	print(i)
-# 	g.write(i + "\n")
-# f.close()
-# g.close()
+f.close();
 
-
-f = open("resultat_1h30")
+f = open(input_file)
+#récupération des edges
 for i in f:
 	tab = i.partition(";")
 	if (tab[0] in categories) and (tab[2].rstrip() in categories):
 		src = categories.index(tab[0])
 		dst = categories.index(tab[2].rstrip())
 		links.append((src, dst))
-# print(nodes)
 
 
+#fichier de sortie
+f = open(output_file, "wb")
 
-f = open("amazon_gephi.gexf", "wb")
+#ajout de l'entete
+# debut ==========================================================================
 f.write(bytes("<?xml version='1.0' encoding='UTF-8'?>", 'UTF-8'))
 root = etree.Element("gexf")
 root.set("xmlns", "http://www.gexf.net/1.2draft")
 root.set("version", "1.2")
+
 meta = etree.SubElement(root, "meta")
 meta.set("lastmodifieddate", "2009-03-20")
+
 creator = etree.SubElement(meta, "creator")
 creator.text = "Gexf.net"
+
 description = etree.SubElement(meta, "description")
-description.text = "Olympique Lyonnais"
+description.text = "description"
 
 graph = etree.SubElement(root, "graph")
 graph.set("defaultedgetype", "undirected")
@@ -57,7 +62,10 @@ attribute.set("mode", "static")
 attributenode = etree.SubElement(graph, "attributes")
 attributenode.set("class", "node")
 attributenode.set("mode", "static")
+# fin entete ==========================================================================
 
+
+#ajout des nodes
 nodes = etree.SubElement(root, "nodes")
 
 counter = 0
@@ -67,6 +75,8 @@ for i in categories:
 	node.set("label", i)
 	counter += 1
 
+
+#ajout des edges
 edges = etree.SubElement(root, "edges")
 counter = 0
 for i in links:
@@ -77,11 +87,13 @@ for i in links:
 	counter += 1
 
 
+#ecriture dans le fichier
 f.write(etree.tostring(root, pretty_print=True))
 
 
 
 # example gephi gefx simple file
+#
 # <?xml version="1.0" encoding="UTF-8"?>
 # <gexf xmlns="http://www.gexf.net/1.2draft" version="1.2">
 #     <meta lastmodifieddate="2009-03-20">
